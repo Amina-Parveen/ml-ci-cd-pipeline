3d3169028b2736b9d9e8a6edc498c50559c64986
Amina Parveen <aminaparveen2205@gmail.com>
2025-08-27 01:18:30 +0530

    Fix species with target

--- model/evaluate.py
+++ model/evaluate.py
@@ -2,13 +2,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import accuracy_score
 import joblib
+
 # Load the dataset
 
 data = pd.read_csv('data/iris.csv')
 
 # Preprocess the dataset
-X = data.drop('species', axis=1) 
-y = data['species']
+X = data.drop('target', axis=1) 
+y = data['target']
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
